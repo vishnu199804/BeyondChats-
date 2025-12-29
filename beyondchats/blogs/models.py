6e3ad30e026291d3709c01bfb6e56ff84cf200fc
vishnu199804 <vishnuramesh1804@gmail.com>
2025-12-29 11:02:23 +0530

    Article model created

---
+++ beyondchats/blogs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    source_url = models.URLField()
+    published_date = models.DateField(null=True,blank=True)
+    is_updates = models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
